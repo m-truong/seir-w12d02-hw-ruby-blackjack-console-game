9bc78232ac5177ed811445163d47b3e998213abc
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-13 22:45:49 -0500

    completed BlackJack and completed extra Singly Linked List assignment
    I went through the singly linked list markdown, added comments with my understanding, and also ran the code inside a repl for extra data-structures practice.

--- blackjack.rb
+++ blackjack.rb
@@ -1,24 +1,124 @@
-deck = []
-
-
 class Player 
-    attr_accessor :name, :bankroll, :hand
-    def initialize (name:, bankroll:, hand:)
+    attr_accessor :name, :hand
+    def initialize (name="Player")
         @name = name
-        @bankroll = 100
-        @hand = []
+        @hand = nil
+        # Note: I practiced YAGNI here, and I decided it wasn't necessary to implement the 'bankroll' property since the game ends after a single round.
     end 
 end 
-
 class Computer 
-    attr_accessor :name, :bankroll, :hand
-    def initialize (name:, bankroll:, hand:)
+    attr_accessor :name, :hand
+    def initialize (name="House")
         @name = name
-        @bankroll = 10000
-        @hand = []
+        @hand = nil
     end 
 end 
-
-# p "What is your name?"
-# name = gets.chomp
-# p "Your name is #{name}"
+class GameState
+    def initialize 
+        @game_running = true
+        # Note: Due to YAGNI, it made more sense to just hardcode a numbers array from 2-11.
+        # Note: This approach made more sense because the 'face' cards Jack, Queen, and King are values of '10' anyways, so it made more logical sense to just hard code them into the deck array.
+        @deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+            2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+        # This creates Player class.
+        @player = Player.new 
+        # This assigns the Player.hand property to an array of two cards.
+        @player.hand = draws_two_cards
+        # This does the same for the Computer class.
+        @house = Computer.new
+        @house.hand = draws_two_cards
+        assign_player_name
+        # This game_start 'while-loop' is called inside constructor method once the GameState class is initialized at the very bottom of the file.
+        game_start
+    end 
+    def assign_player_name
+        # This assigns player's name.
+        p "🃏🃏🃏 Welcome to Ruby BlackJack Console Game! 🃏🃏🃏"
+        p "Please input your desired BlackJack player name in the console: "
+        @player.name = gets.chomp
+        p "Your name is #{@player.name}!"
+    end
+    # This prints the current hand sum for the player and the house. 
+    def print_current_hands
+        p "#{@player.name}'s current hand: #{@player.hand}, and the sum is #{get_hand_sum(@player.hand)}"
+        p "#{@house.name} current hand: #{@house.hand}, and the sum is #{get_hand_sum(@house.hand)}"
+    end 
+    # This starts the game and continues looping until the user's input is 'stay'.
+    def game_start 
+        # Default-value for user_input to start game.
+        user_input = false 
+        while user_input != 'stay'
+            p "#{@player.name} please choose to 'hit' or 'stay'?" 
+            print_current_hands
+            if get_hand_sum(@player.hand) > 21 
+                p "❌ Sorry #{@player.name} you Busted! #{@house.name} has won! Game Over! Please Play Again! ❌"
+                # This is an explicit return that doesn't need need a ';'.
+                return 
+            end 
+            user_input = gets.chomp
+            # This checks if the player's input was 'hit' or 'stay'. 
+            if user_input == 'hit'
+                add_card_to_hand(@player.hand)
+            elsif user_input == 'stay'
+                p "#{@player.name} has chosen to stay!" 
+            # This prints when the user types something else other than 'hit' or 'stay' inside the terminal. 
+            else 
+                p "Sorry, #{@player.name}! That's an invalid input! Please type in 'hit' or 'stay'!"
+            end 
+        end 
+        # Once the while loop stops, the 'house_hits_loop' runs until one of the "win/lose conditions is met."
+        house_hits_loop
+        # This checks the game winning conditions.
+        check_winning_state
+    end 
+    def house_hits_loop
+        # The 'house' player continues to 'hit', but won't go over a sum 'value' of 17.
+        while get_hand_sum(@house.hand) < 17
+            p "#{@house.name} hits!"
+            add_card_to_hand(@house.hand)
+            print_current_hands
+        end 
+    end 
+    def check_winning_state
+        print_current_hands
+        # This checks first if player sum is greater than 21...
+        if get_hand_sum(@player.hand) > 21
+            p "❌ Sorry #{@player.name} you Busted! #{@house.name} has won! Game Over! Please Play Again! ❌"
+        # then it checks if house sum is greater than 21...
+        elsif get_hand_sum(@house.hand) > 21 
+            p "🎉 Congratulations #{@player.name}! You Won! #{@house.name} busted! Please Play Again! 🎉"
+        # then it checks if there's been a tie...
+        elsif get_hand_sum(@player.hand) == get_hand_sum(@house.hand) 
+            p "🃏🃏🃏 There was a draw! Game Over! 🃏🃏🃏"
+        # then it checks if player's hand is greater than house's hand...
+        elsif get_hand_sum(@player.hand) > get_hand_sum(@house.hand) 
+            p "🎉 Congratulations #{@player.name}! You Won! #{@house.name} busted! Please Play Again! 🎉"
+        # then it checks if house's hand is greater than player's hand...
+        elsif get_hand_sum(@player.hand) < get_hand_sum(@house.hand)
+            p "❌ Sorry #{@player.name} you Busted! #{@house.name} has won! Game Over! Please Play Again! ❌"
+        # This is a catch all print statement for an error in the game logic.
+        else 
+            p "Something went wrong with the BlackJack game logic!"
+        end 
+    end 
+    # Adds a card to either player's hand array.
+    def add_card_to_hand(curr_hand)
+        curr_hand.push(get_rand_card)
+    end 
+    # This sums the hand array.
+    def get_hand_sum(curr_hand) 
+        curr_hand.inject(0) { |sum, card| sum + card }
+    end 
+    # This initially generates two random numbers from the deck array and creates a two element array of cards.
+    def draws_two_cards 
+        [get_rand_card, get_rand_card]
+    end 
+    # This grabs a random number element from the @deck array as a random card value. 
+    def get_rand_card 
+        @deck[rand(@deck.length)]
+    end 
+end
+# Main Driver Method Starts Game
+GameState.new
--- sll.md
+++ sll.md
@@ -16,8 +16,12 @@ A linked list is formed when many such nodes are linked together to form a chain
 ```ruby
 # Full list and methods
 class LinkedList
+    # gets-sets :head-:tail nodes
+    # adds :size property for length of SLL 
     attr_accessor :head, :tail, :size 
   
+  # SLL initialized with head-nil and tail-nil
+  # size of SLL is 0 
     def initialize
       @head = nil
       @tail = nil
@@ -25,16 +29,24 @@ class LinkedList
     end
   
     # this append values to the end of the SLL
+    # append-method adds new Node-object-instance to end of SLL object
     def append(value)
+    # instantiates new Node-object and passes in value  
       node = Node.new(value)
+      # increments @size instance of SLL object by one
       @size += 1
-  
+
+      # hits this if statement once 
+      # checks if has head Node yet
       if @head.nil?
         @head = node
       else
+      # calling next_node property on the existing-Node
+      # sets equal to new Node object 
         @tail.next_node = node
       end
-  
+
+      # assumes that the tail is also the same Node? 
       @tail = node
     end
   
@@ -48,18 +60,21 @@ class LinkedList
       else
         node.next_node = @head
       end
-  
+    # changes the 'pointers' so that the @head is this new node 
       @head = node
     end
   
     # Returns the node at the given index.
     def at(index)
+    # traversal 
         travel = @head
+        # pass in num <= length of SLL
         if index <= @size
+          # traverses the nodes by reasisnging the travel-node to the next-node
             0.upto(index - 1) do |i|
                 travel = travel.next_node
             end
-            
+            # explicit return of the traveled-to node 
             return travel
         else
             return nil
@@ -69,16 +84,21 @@ class LinkedList
   
     # Inserts the data at the given index.
     def insert_at(index, value)
+    # newNode to insert 
         newNode = Node.new(value)
         
         travel = @head
         trail = nil
         if index <= @size
             0.upto(index - 1) do |i|
+            # this maintains pointers
                 trail = travel
+                # keeps reassigning so that it doesn't lose the Nodes 'pointers' 
                 travel = travel.next_node
             end
+            # this connects the Nodes 
             trail.next_node = newNode
+            # insert newNode inbetween the 'trailing node' and 'travel node' 
             newNode.next_node = travel
             @size += 1
         end
@@ -93,6 +113,9 @@ class LinkedList
                 trail = travel
                 travel = travel.next_node
             end
+            # this skips the SLL connection of the IN-BETWEEN node 
+            # thus losing the reference pointer 
+            # where does it go in memory though?
             trail.next_node = travel.next_node
             @size -= 1
         end
@@ -101,15 +124,23 @@ class LinkedList
   
     # Removes and returns the last element from the list.
     def pop
+    # edgecase for if no nodes inserted into SLL yet 
+    # prevents pop from executing 
+    # checks if SLL has a tail-node then executes 
         if @tail != nil
+        # stores to return the Node 
             old_tail = @tail
 
+            # calls helper-function to get node previous to this one 
             new_tail = at(@size - 2)
+            # changes the last-nodes.next-node to be nil 
             new_tail.next_node = nil
 
+            # then changes the SLL @tail property to the new_tail Node 
             @tail = new_tail
             @size -= 1
 
+            # outputs the old_tail Node 
             return old_tail
         end
     end
@@ -128,29 +159,40 @@ class LinkedList
     end
   
     # Returns the index of the node containing data, or nil if not found.
-  
+    # searches for the Node containing this (data) e.g. "string" or integer
     def find(data)
+    # stores index, and returns index once found! 
         index = nil
+        # "travelled-Node!"
         travel = @head
         0.upto(@size - 1) do |i|
+        # Base-Case 
+        # if travel-node value matches the 'data' searching for then it explicitly returns
             if(travel.value == data)
                 index = i
             end
             travel = travel.next_node
         end
+        # returns index 
         return index
     end
     #overload display
+    # method overriding to_s in Ruby  
     def to_s
       string = ''
-      
+      # pushes all the nodes_values_to_s into the string 
       current_node = @head
       while(current_node != nil)
         string << current_node.value.to_s
+        # pushes 'skinny-arrow' into string
         string << '->'
+        # reassigns to next node 
         current_node = current_node.next_node
       end
+      # adds 'nil'
       string << 'nil'
+      # implicit return that returns the string variable 
+      # # # # 
       string
     end
   end
@@ -169,6 +211,7 @@ end
 ```
 
 ```ruby
+# can just invoke the initializer() 
 list = LinkedList.new
 
 list.append(3)
